5b9f87c0c0b407c9cd93d930d10c41ddd3edd452
Cory.Lanza <Cory.Lanza@roadscholar.org>
2020-12-27 09:19:49 -0500

    Add project files.

---
+++ Core/Shared/Components/TimePicker.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Shared.Components
+{
+    public partial class TimePicker
+    {
+        private const string AM = "AM";
+        private const string PM = "PM";
+
+        private int HourPicked { get; set; }
+        private int Minute { get; set; }
+        private string AmOrPm { get; set; }
+
+        public TimeSpan Time => new TimeSpan(AmOrPm == AM ? HourPicked : HourPicked + 12, Minute, 0);
+
+    }
+}
