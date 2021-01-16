041aaf52faf25ac8a80bfb071fce69330f4acf8c
Cory.Lanza <Cory.Lanza@roadscholar.org>
2021-01-16 15:38:55 -0500

    Added outlet editor, material icons, and some more dev

---
+++ Core/Pages/OutletLang.razor.cs
@@ -0,0 +1,89 @@
+﻿using Core.Shared.Components;
+using Outlet;
+using Outlet.Operands;
+using Outlet.StandardLib;
+using Outlet.TreeViewer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Pages
+{
+    public partial class OutletLang
+    {
+        [NotNull]
+        // value set in razor component, will not be null
+        private OutletEditor? Editor { get; set; }
+
+        public List<(string code, List<string> consoleOutput, Operand? result)> History = new List<(string, List<string>, Operand?)>();
+
+        private const string success = "text-success";
+        private const string failure = "text-danger";
+
+        private string CheckCssClass = "";
+
+        private ReplOutletProgram Program { get; set; }
+
+        //private Node? AST { get; set; }
+
+        public OutletLang()
+        {
+            var browserInterface = new SystemInterface(
+                stdin: GetInput,
+                stdout: ShowOutput,
+                stderr: ShowError
+            );
+            Program = new ReplOutletProgram(browserInterface);
+        }
+
+        protected List<string> Errors { get; set; } = new List<string>();
+
+        public async Task RunOutlet()
+        {
+            var input = await Editor.GetValue();
+            Errors.Clear();
+            var result = Program.Run(Encoding.ASCII.GetBytes(input));
+            if(!Errors.Any())
+            {
+                History.Add((input, new List<string>(), null));
+                var last = History.Last();
+                last.result = result;
+                History.RemoveAt(History.Count - 1);
+                History.Add(last);
+            }
+        }
+
+        public async Task CheckOutlet()
+        {
+            var input = await Editor.GetValue();
+            Errors.Clear();
+            Program.Check(Encoding.ASCII.GetBytes(input));
+
+            CheckCssClass = Errors.Count > 0 ? failure : success;
+        }
+
+        public void ShowError(Exception ex)
+        {
+            Errors.Add(ex.Message);
+        }
+
+        public void ShowOutput(string output)
+        {
+            var (code, consoleOutput, result) = History.Last();
+            consoleOutput.Add(output);
+        }
+
+        public string GetInput()
+        {
+            return "";
+        }
+
+        //public void ShowAST()
+        //{
+        //    AST = Program.GenerateAST();
+        //}
+    }
+}
---
+++ Core/Shared/Components/OutletEditor.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Shared.Components
+{
+    public partial class OutletEditor
+    {
+        [Parameter]
+        public string? ID { get; init; }
+        public async Task<string> GetValue() => await JSRuntime.InvokeAsync<string>("getCodeEditorValue", new object[] { ID ?? "editor" });
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeAsync<string>("highlightCode", new object[] { ID ?? "editor" });
+            }
+        }
+    }
+}
