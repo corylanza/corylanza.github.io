2646c3b6e71dd791a1a58f9004dae98df3fb2dd9
Cory.Lanza <Cory.Lanza@roadscholar.org>
2021-01-18 08:12:02 -0500

    misc work on sleep time clone and starting web compile

--- Core/Pages/SleepTimeClone.razor.cs
+++ Core/Pages/SleepTimeClone.razor.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 using Core.Shared.Components;
+using Microsoft.AspNetCore.Components;
 
 namespace Core.Pages
 {
@@ -11,20 +11,24 @@ namespace Core.Pages
         private TimePicker Time1;
         private TimePicker Time2;
 
-        private readonly List<TimeSpan> Results = new List<TimeSpan>();
+        private float HoursToSleep { get; set; }
 
-        private void Calculate()
-        {
-            Results.Clear();
-            Results.Add(Time1.Time.Subtract(new TimeSpan(1, 30, 0)));
-            Results.Add(Time1.Time.Subtract(new TimeSpan(3, 00, 0)));
-            Results.Add(Time1.Time.Subtract(new TimeSpan(4, 30, 0)));
-            Results.Add(Time1.Time.Subtract(new TimeSpan(6, 00, 0)));
+        private static IEnumerable<(TimeSpan time, string cssClass)> Intervals {
+            get {
+                yield return (new TimeSpan(1, 30, 0), "text-warning");
+                yield return (new TimeSpan(3, 00, 0), "text-warning");
+                yield return (new TimeSpan(4, 30, 0), "text-info");
+                yield return (new TimeSpan(6, 00, 0), "text-info");
+                yield return (new TimeSpan(7, 30, 0), "text-success");
+                yield return (new TimeSpan(9, 00, 0), "text-success");
+            }
         }
 
-        private void WhenToWakeUpIfYouFallAsleepAt(TimeSpan date)
-        {
+        // Needed because TimeSpan string formatting isn't great,
+        // instead use any DateTime
+        private readonly DateTimeOffset aDate = new DateTime(2020, 01, 01);
+        private string TwelveHourTime(TimeSpan ts) => aDate.Add(ts).ToString("hh:mm tt");
 
-        }
+        private RenderFragment? Result { get; set; }
     }
 }
